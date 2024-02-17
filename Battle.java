ce38c8f0ec73435320024cf3819230a1631b5a51
Artemy Gibson <artemygibson@gmail.com>
2024-02-17 15:11:45 -0800

    Build methods for taking random coordinates for computer ship placement

--- Battle.java
+++ Battle.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -44,18 +45,69 @@ public class Battle {
   public void place_computer_ships() {
     
     Object[] computer_board_keys = computer_board.cells().keySet().toArray();
-    Object key = computer_board_keys[new Random().nextInt(computer_board_keys.length)];
-    int direction = new Random().nextInt(2);
-    if (direction == 0) {
-      take_horizontal_coordinates(key); // plus ship length as argument
-    } else {
-
+    for (Ship ship : computer_ships) {
+      Object key = computer_board_keys[new Random().nextInt(computer_board_keys.length)];
+      int direction = new Random().nextInt(2);
+      if (direction == 0) {
+        String[] coords = new String[ship.length];
+        do {
+          coords = take_horizontal_coordinates(key, ship.length);
+        }
+        while (!computer_board.valid_placement(ship, coords));
+        computer_board.place_ship(ship, coords);
+      } else {
+        String[] coords = new String[ship.length];
+        do {
+          coords = take_vertical_coordinates(key, ship.length);
+        }
+        while (!computer_board.valid_placement(ship, coords));
+        computer_board.place_ship(ship, coords);
+      }
     }
   }
 
-  public void take_horizontal_coordinates(Object starter_coordinate) {
-    
+  public String[] take_horizontal_coordinates(Object starter_coordinate, int ship_length) {
+    // takes in starter coordinate and ship length, which represents how many additional coordinates to take
+    String coordinate = starter_coordinate.toString();
+    // split the starter coordinate into letter and integer
+    String letter = Character.toString(coordinate.charAt(0));
+    int number = Character.getNumericValue(coordinate.charAt(1));
+    // put starter coordinate into an array of length determined by ship length
+    String[] coordinates = new String[ship_length];
+    Array.set(coordinates, 0, coordinate);
+
+    // for as many times as the number representing the length of the ship minus one
+    for (int i = 1; i < ship_length; i++) {
+      // take the last char of the starter coordinate (an integer)
+      // increase it to modify the original coordinate and add that to array at appropriate index
+      int num = number + i;
+      String next_coord = letter + num;
+      coordinates[i] = next_coord;
+    }
+    return coordinates;
+    // return an array of string elements representing coordinates
   }
+
+  public String[] take_vertical_coordinates(Object starter_coordinate, int ship_length) {
+    String coordinate = starter_coordinate.toString();
+    // split the starter coordinate into letter and integer
+    char letter = coordinate.charAt(0);
+    int number = Character.getNumericValue(coordinate.charAt(1));
+    // put starter coordinate into an array of length determined by ship length
+    String[] coordinates = new String[ship_length]; // placeholder ship length
+    coordinates[0] = coordinate;
+    // for as many times as the number representing the length of the ship minus one
+    for (int i = 1; i < ship_length; i++) {
+      // take the first char of the starter coordinate (letter)
+      // increase it to modify the original coordinate and add that to array at appropriate index
+      // String next_coord = Character.toString(letter) + (number + i);
+      String next_letter = Character.toString(letter + i);
+      String next_coord = next_letter + Integer.toString(number);
+      coordinates[i] = next_coord;
+    }
+    return coordinates;
+  }
+
 
   public void place_user_ships() {
 
