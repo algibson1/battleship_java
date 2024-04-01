2c825f0e689265ffeec65a4efbd10b858a80ea24
Artemy Gibson <artemygibson@gmail.com>
2024-04-01 16:08:36 -0700

    Reorganize structure of test document to be more conventional

--- BattleTest.java
+++ BattleTest.java
@@ -1,25 +1,42 @@
 import org.junit.*;
 
 public class BattleTest {
+  Battle battle = new Battle();
+  String[] horizontalCoords1 = new String[] {"A1", "A2", "A3"};
+  String[] horizontalCoords2 = new String[] {"B3", "B4"};
+  String[] vertCoords1 = new String[] {"A1", "B1", "C1"};
+  String[] vertCoords2 = new String[] {"B3", "C3"};
+
+  // start() cannot be tested because it has no return value;
+  // It prints things to terminal
+
+  // gameSetup() cannot be tested because it is a void method
+  // it prints things to terminal and calls other methods
+
   @Test
-  public void battle_test() {
-    Battle battle = new Battle();
-    // battle.start();
-    // Assert.assertEquals("Welcome to Battleship", battle.welcome());
-    // Assert output to terminal?
-    // Assert.assertEquals("")
-
-    //test take_horizontal_coordinates
-    String[] horizontal_coords_1 = new String[] {"A1", "A2", "A3"};
-    String[] horizontal_coords_2 = new String[] {"B3", "B4"};
-    Assert.assertArrayEquals(horizontal_coords_1, battle.take_horizontal_coordinates("A1", 3));
-    Assert.assertArrayEquals(horizontal_coords_2, battle.take_horizontal_coordinates("B3", 2));
-
-
-    //test take_vertical_coordinates
-    String[] vert_coords_1 = new String[] {"A1", "B1", "C1"};
-    String[] vert_coords_2 = new String[] {"B3", "C3"};
-    Assert.assertArrayEquals(vert_coords_1, battle.take_vertical_coordinates("A1", 3));
-    Assert.assertArrayEquals(vert_coords_2, battle.take_vertical_coordinates("B3", 2));
+  public void placeComputerShips_PlacesShipsOnComputerBoard() {
+    // first prove that the computer's board is blank (all cells empty)
+    // Run method
+    // Prove that two ships are on the computer's board (5 cells are occupied)
   }
+
+  // Break down placeComputerShips into several helper methods later,
+  // test each
+
+  @Test
+  public void takeHorizontalCoordinates_FindsValidHorizontalCoordinatesToPlaceShip() {
+    Assert.assertArrayEquals(horizontalCoords1, battle.takeHorizontalCoordinates("A1", 3));
+    Assert.assertArrayEquals(horizontalCoords2, battle.takeHorizontalCoordinates("B3", 2));
+  }
+
+  @Test
+  public void takeVerticalCoordinates_FindsValidVerticalCoordinatesToPlaceShip() {
+    Assert.assertArrayEquals(vertCoords1, battle.takeVerticalCoordinates("A1", 3));
+    Assert.assertArrayEquals(vertCoords2, battle.takeVerticalCoordinates("B3", 2));
+  }
+
+  // @Test
+  // public void placeUserShips_PlacesUserShips() {
+  //   // find a way to test a method that needs user input?
+  // }
 }
